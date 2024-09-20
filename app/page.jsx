74efe6e6650fed4dae3bae5088d61329412f8271
vwut <vwut@academic.rrc.ca>
2024-09-20 10:02:47 -0500

    Start page populated with header and slideshow

--- app/page.jsx
+++ app/page.jsx
@@ -1,29 +1,53 @@
-import Link from "next/link";
+import Link from 'next/link';
+
+import classes from './page.module.css';
+import ImageSlideshow from '@/components/images/image-slideshow';
 
 export default function Home() {
   return (
-    <main>
-      <header style={{ display: "inline-flex" }}>
-        <h1 style={{ color: "white", textAlign: "center" }}>
-          Time to get started!
-        </h1>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+          <ImageSlideshow/>
+        </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste and share food from all over the world.</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
       </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
 
-      <h2 style={{ textAlign: "center" }}>
-        <Link href="/meals" className="link">
-          Meals
-        </Link>
-      </h2>
-      <h2>
-        <Link href="meals/share" className="link">
-          Share your meals
-        </Link>
-      </h2>
-      <h2>
-        <Link href="/community" className="link">
-          Community
-        </Link>
-      </h2>
-    </main>
+        <section className={classes.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
--- components/main-header.jsx
+++ components/main-header.jsx
@@ -4,22 +4,27 @@ import Image from "next/image";
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 
-export default function MainHeader() {
-    return <header className={classes.header}>
-        <Link href='/' className={classes.logo}>
-            <Image src={logoImg} alt="A plate with food on it" />
-            NextLevel Food
-        </Link>
+import MainHeaderBackground from "./main-header-background";
 
-        <nav className={classes.nav}>
-            <ul>
-                <li>
-                    <Link href='/meals'>Browse Meals</Link>
-                </li>
-                <li>
-                    <Link href='/commmunity'>Foodies Community</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
+export default function MainHeader() {
+    return (<>
+        <MainHeaderBackground />
+        <header className={classes.header}>
+            <Link href='/' className={classes.logo}>
+                <Image src={logoImg} alt="A plate with food on it" priority />
+                NextLevel Food
+            </Link>
+
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Link href='/meals'>Browse Meals</Link>
+                    </li>
+                    <li>
+                        <Link href='/commmunity'>Foodies Community</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </>)
 }
